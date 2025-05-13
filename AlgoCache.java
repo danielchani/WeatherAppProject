356aa7a910f1c3140ae3800c34af83663ad4d1ee
danielchani <116919990+danielchani@users.noreply.github.com>
2025-05-13 18:40:34 +0300

    Update AlgoCache.java

--- AlgoCache.java
+++ AlgoCache.java
@@ -5,5 +5,5 @@ public interface AlgoCache {
     void requestPage(int pageNumber); /** requesting page from mem to load */
 
     boolean contains(int pageNumber); /** checks if the page exists in cache, true or false */
-ffadfadasd
+
 }
